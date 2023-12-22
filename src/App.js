bcacd8a0d44727448cd44980d14c1c7508af6637
1nc0gn30 <br34kth3m4tr1xn30@gmail.com>
2023-12-22 12:10:25 -0500

    3

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Camera from './components/Camera';
 import LicensePlateScanner from './components/LicensePlateScanner';
-import TokenDisplay from './components/TokenDisplay';
-import ErrorComponent from './components/ErrorComponent';
 
-function App() {
+const App = () => {
   const [capturedImage, setCapturedImage] = useState(null);
 
   const handleCapture = (imageSrc) => {
     setCapturedImage(imageSrc);
-    // Optionally, navigate to the /scan route if needed
   };
 
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Camera onCapture={handleCapture} />} />
-          <Route path="/scan" element={<LicensePlateScanner image={capturedImage} />} />
-          <Route path="/token" element={<TokenDisplay />} />
-          <Route path="/error" element={<ErrorComponent />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Camera onCapture={handleCapture} />
+      {capturedImage && <LicensePlateScanner image={capturedImage} />}
+    </div>
   );
-}
+};
 
 export default App;
